6a2f81349454e56aa9338d726ed9c49ed9cacb61
hashmi <your-email@hlpl.com>
2026-04-24 19:51:34 +0530

    Add simple test page at root level

---
+++ app/test-page/page.tsx
@@ -0,0 +1,8 @@
+export default function SimpleTest() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, routing is working correctly.</p>
+    </div>
+  )
+}
